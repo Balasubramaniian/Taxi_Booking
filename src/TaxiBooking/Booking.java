5d6b944b660c6468716c73eb61ce1001d13ee05c
S.Balasubramaniyan <balasubramaniyan.sel@gmail.com>
2024-03-25 22:09:07 +0530

    ThirdCommit

--- src/TaxiBooking/Booking.java
+++ src/TaxiBooking/Booking.java
@@ -2,6 +2,7 @@ package TaxiBooking;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Booking {
 	
@@ -17,6 +18,30 @@ public class Booking {
 	
 	public static void main(String[] args) {
 		List<Taxi> taxis=createTaxis(4);
+		Scanner sc=new Scanner(System.in);
+		boolean ans=true;
+		int id=1;
+		while(ans) {
+			System.out.println("1-booking 2-display 3-exit");
+			int choice=sc.nextInt();
+			
+			switch(choice) {
+			case 1:
+				int custid=id;
+				System.out.println("Enter the Pickup Point");
+				char pickuppoint=sc.next().toUpperCase().charAt(0);
+				System.out.println("Enter the Drop Point");
+				char droppoint=sc.next().toUpperCase().charAt(0);
+				System.out.println("Enter the Pickup time");
+				int pickuptime=sc.nextInt();
+				
+				if(pickuppoint<'A' || droppoint>='F' && pickuppoint>='F' || droppoint<'A') {
+					System.out.println("Valid pickup points A to F only");
+				}
+//				List<Taxi> freeTaxi=getFreeTaxies(taxis,pickuppoint,pickuptime);
+			}
+			
+		}
 	}
 	
 
